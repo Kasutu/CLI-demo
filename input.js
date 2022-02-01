55d43cdaba482454316298be54669648909d0e24
PZYCO20 <64959978+PZYCO20@users.noreply.github.com>
2022-02-01 21:37:54 +0800

    good comments

--- input.js
+++ input.js
@@ -3,14 +3,24 @@
 // Albus Wulfric Percival Bryan Dumbledore => AWPBD
 // Regulus Arcturus Black => RAB
 
+// import readline using `npm install readline-sync`
 import readline from 'readline-sync';
 
+// make a question and split everything with a ' ' (space) seperator
+// to make an array of words
 const name = readline.question('name? ').split(' ');
+console.log(name);
+
 const initials = [];
 
+// loop through the array and find the first index and push it to an array
+// or just `initials += initial` add it to a string
+// Richard Michael Coo => ['Richard', 'Michael', 'Coo']
+// outputs -> RMC___________R__________M__________C
 name.forEach((element) => {
 	let initial = element[0].toUpperCase();
 	initials.push(initial);
 });
 
+// Output
 console.log('output -> ', initials.join(''));
